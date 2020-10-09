2f163e71f27453c5d9c4cd9d584a6f26216639bb
YXX <3283573415@qq.com>
2020-10-09 09:41:30 +0800

    第二次提交项目

---
+++ src/script/lunbo.js
@@ -0,0 +1,10 @@
+define([], function() {
+    return {
+        lunbo: ! function() {
+            // let length = document.body.clientWidth;
+            // const lunbo_container = $('.lunbo-container');
+            // lunbo_container.css('width', length);
+            // console.log(2);
+        }()
+    }
+})
---
+++ src/script/main.js
@@ -0,0 +1,24 @@
+require.config({
+    // baseUrl: '', //公共的路径，基路径，所有模块共有的路径
+    paths: {
+        'jquery': 'https://cdn.bootcdn.net/ajax/libs/jquery/1.12.4/jquery.min',
+        'jq_cookie': 'https://cdn.bootcdn.net/ajax/libs/jquery-cookie/1.0/jquery.cookie.min',
+        'jq_lazyload': 'https://cdn.bootcdn.net/ajax/libs/jquery.lazyload/1.8.3/jquery.lazyload.min'
+    },
+    shim: { //不支持AMD模块，支持AMD
+        'jq_cookie': {
+            deps: ['jquery'], //模块的依赖
+            exports: 'jq_cookie'
+        },
+        'jq_lazyload': {
+            deps: ['jquery'], //模块的依赖
+            exports: 'jq_lazyload'
+        }
+    }
+});
+
+// 加载模块
+require(['jquery'], function() {
+    require(['test']);
+    require(['lunbo']);
+});
